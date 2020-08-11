1f4e35ad3a86695b5dc47ef9f00fe8bb328e46e5
Utente <Utente@DESKTOP-73HFT01>
2020-08-11 18:59:32 +0200

    primoCommit

---
+++ src/main/java/it/polito/tdp/camminoAutobus/FXMLController.java
@@ -0,0 +1,30 @@
+package it.polito.tdp.camminoAutobus;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.camminoAutobus.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class FXMLController {
+	
+	private Model model;
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+
+	public void setModel(Model model) {
+		this.model=new Model();
+		
+	}    
+}
---
+++ src/main/java/it/polito/tdp/camminoAutobus/model/Model.java
@@ -0,0 +1,5 @@
+package it.polito.tdp.camminoAutobus.model;
+
+public class Model {
+
+}
